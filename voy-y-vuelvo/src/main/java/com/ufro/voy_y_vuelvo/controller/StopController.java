fb928667c6755d8a476b5ced19a59cd480001054
AdolfoGayoso <a.gayoso02@ufromail.cl>
2025-05-15 02:48:53 -0400

    avance

--- voy-y-vuelvo/src/main/java/com/ufro/voy_y_vuelvo/controller/StopController.java
+++ voy-y-vuelvo/src/main/java/com/ufro/voy_y_vuelvo/controller/StopController.java
@@ -1,9 +1,26 @@
 package com.ufro.voy_y_vuelvo.controller;
 
+import com.ufro.voy_y_vuelvo.dto.ApiResponse;
+import com.ufro.voy_y_vuelvo.dto.StopDto;
+import com.ufro.voy_y_vuelvo.service.StopService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/stop")
+@RequiredArgsConstructor
 public class StopController {
+    private final StopService stopService;
+
+    @GetMapping("/get-all")
+    public ResponseEntity<ApiResponse<?>> getAllStops() {
+        ApiResponse<?> response = new ApiResponse<>(HttpStatus.OK.value(), "Paradas obtenidas con exito.", stopService.getAllStops());
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 }
--- voy-y-vuelvo/src/main/java/com/ufro/voy_y_vuelvo/service/StopService.java
+++ voy-y-vuelvo/src/main/java/com/ufro/voy_y_vuelvo/service/StopService.java
@@ -1,7 +1,39 @@
 package com.ufro.voy_y_vuelvo.service;
 
+import com.ufro.voy_y_vuelvo.dto.ApiResponse;
+import com.ufro.voy_y_vuelvo.dto.StopDto;
+import com.ufro.voy_y_vuelvo.model.trips.Stop;
+import com.ufro.voy_y_vuelvo.repository.StopRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class StopService {
+
+    private final StopRepository stopRepository;
+
+    public List<StopDto> getAllStops() {
+        return stopRepository.findAll()
+                .stream()
+                .map(this::toDto)
+                .toList();
+    }
+
+    public StopDto findByName(String name) {
+
+        StopDto  stopDto = 
+
+
+    }
+
+    public StopDto toDto(Stop stop) {
+        StopDto stopDto = new StopDto();
+        stopDto.setId(stop.getId());
+        stopDto.setName(stop.getName());
+        return stopDto;
+    }
 }
