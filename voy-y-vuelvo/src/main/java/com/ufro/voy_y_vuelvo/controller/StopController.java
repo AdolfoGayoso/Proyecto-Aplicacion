db9b22c7035d83067c9ba088a0112c4bbe2ede41
AdolfoGayoso <a.gayoso02@ufromail.cl>
2025-05-25 18:53:43 -0400

    controlador obtener paradas

--- voy-y-vuelvo/src/main/java/com/ufro/voy_y_vuelvo/controller/StopController.java
+++ voy-y-vuelvo/src/main/java/com/ufro/voy_y_vuelvo/controller/StopController.java
@@ -1,18 +1,13 @@
 package com.ufro.voy_y_vuelvo.controller;
 
 import com.ufro.voy_y_vuelvo.dto.ApiResponse;
-import com.ufro.voy_y_vuelvo.dto.StopDto;
 import com.ufro.voy_y_vuelvo.service.StopService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/stop")
@@ -26,12 +21,4 @@ public class StopController {
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    @GetMapping("/get-stop-by-name")
-    public ResponseEntity<ApiResponse<?>> getStopByName(@RequestParam String name) {
-        ApiResponse<?> response = new ApiResponse<>();
-        Optional<StopDto> stopDto = stopService.findByName(name);
-
-
-
-    }
 }
